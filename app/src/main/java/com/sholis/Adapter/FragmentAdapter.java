5a8a7b51a8dc9d76f9cafa1f341f8a14b0fdcdb1
Cargozin <74237674+Cargozin@users.noreply.github.com>
2021-09-12 13:38:29 +0200

    feat: implemented TabLayout Nav in MainActivity (not properly working atm)

---
+++ app/src/main/java/com/sholis/Adapter/FragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.sholis.Adapter;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.Lifecycle;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.sholis.Fragments.*;
+
+public class FragmentAdapter extends FragmentStateAdapter {
+
+    private int TabCount;
+
+    public FragmentAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle lifecycle, int CountTab){
+        super(fragmentManager, lifecycle);
+        this.TabCount = CountTab;
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch(position){
+            case 0:
+                return new Tab1();
+            case 1:
+                return new Tab2();
+            case 2:
+                return new Tab3();
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return TabCount;
+    }
+}
--- app/src/main/java/com/sholis/MainActivity.java
+++ app/src/main/java/com/sholis/MainActivity.java
@@ -1,19 +1,47 @@
 package com.sholis;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
 
+import com.google.android.material.tabs.TabLayout;
+import com.sholis.Adapter.FragmentAdapter;
 import com.sholis.web.WebInterface;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Toolbar toolbar;
+    TabLayout tabLayout;
+    ViewPager2 viewPager2;
+    FragmentAdapter fragmentAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Implementation TabLayout Navigation using ViewPager2
+        toolbar = findViewById(R.id.toolbar);
+        tabLayout = findViewById(R.id.tab_layout);
+        viewPager2 = findViewById(R.id.viewPager2);
+
+        setSupportActionBar(toolbar);
+
+        //name tabs
+        tabLayout.addTab(tabLayout.newTab().setText("Tab1"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab2"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tab3"));
+
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+
+        fragmentAdapter = new FragmentAdapter(getSupportFragmentManager(), getLifecycle(),tabLayout.getTabCount());
+
+        viewPager2.setAdapter(fragmentAdapter);
+
 
         Item banane = new Item(0,"Banane","1");
         Item apfel = new Item(1,"Apfel","1");
