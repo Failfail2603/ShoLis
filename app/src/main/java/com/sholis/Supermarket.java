ac3f7737e1631228f53078ff110a692225b7dfdc
Cargozin <74237674+Cargozin@users.noreply.github.com>
2021-09-13 17:10:36 +0200

    fixed selected Tab color. +Added class Supermarket

---
+++ app/src/main/java/com/sholis/Supermarket.java
@@ -0,0 +1,13 @@
+package com.sholis;
+
+public class Supermarket {
+
+    private String name;
+    private int id;
+
+    public Supermarket(String name, int id){
+        this.name = name;
+        this.id = id;
+    }
+
+}
