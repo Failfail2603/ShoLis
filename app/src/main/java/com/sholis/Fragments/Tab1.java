57b3f4716b0741a5a69fc99a10df6932449d6096
Cargozin <74237674+Cargozin@users.noreply.github.com>
2021-09-15 17:15:52 +0200

    feat: Recyclerview working

--- app/src/main/java/com/sholis/Fragments/Tab1.java
+++ app/src/main/java/com/sholis/Fragments/Tab1.java
@@ -2,65 +2,57 @@ package com.sholis.Fragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.sholis.Adapter.RecyclerViewItemAdapter;
+import com.sholis.Item;
 import com.sholis.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Tab1#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class Tab1 extends Fragment {
+import java.util.ArrayList;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class Tab1 extends Fragment{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    // own parameters
+    RecyclerView recyclerView;
 
-    public Tab1() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Tab1.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Tab1 newInstance(String param1, String param2) {
-        Tab1 fragment = new Tab1();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    @Nullable
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_tab1, container, false);
+        recyclerView = v.findViewById(R.id.recyclerview);
+        System.out.println(recyclerView);
+        recyclerView.setHasFixedSize(true);   //better performance
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManager);
+
+        //testdata
+        Item item1 = new Item(0,"Hallo","20");
+        Item item2 = new Item(1,"Test","5");
+        Item item3 = new Item(2,"Hi","1");
+        Item item4 = new Item(3,"Ok","10");
+        Item item5 = new Item(4,"hifes","7");
+
+
+        ArrayList<Item> items = new ArrayList<>();
+        items.add(item1);
+        items.add(item2);
+        items.add(item3);
+        items.add(item4);
+        items.add(item5);
+
+        RecyclerViewItemAdapter adapter = new RecyclerViewItemAdapter(items);
+        recyclerView.setAdapter(adapter);
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        return v;
     }
 }
