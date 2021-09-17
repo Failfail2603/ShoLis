2bdd3149ca1dd8c97c4a2cdf1edeef88851b09e8
Felix Matthias Krumm <Felix.m.krumm@stud.h-da.de>
2021-09-17 12:26:31 +0200

    feat: inmplemented crude day/night mode

--- app/src/main/java/com/sholis/SettingsActivity.java
+++ app/src/main/java/com/sholis/SettingsActivity.java
@@ -1,10 +1,19 @@
 package com.sholis;
 
+import android.app.UiModeManager;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.PreferenceManager;
+import androidx.preference.SwitchPreferenceCompat;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -22,12 +31,56 @@ public class SettingsActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+
+            SwitchPreferenceCompat darkToggle = findPreference("darkToggle");
+            assert darkToggle != null;
+            //darkToggle.setChecked(loadToggle(getPreferenceManager().getContext()));
+
+
+            darkToggle.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
+                    // Possible Android Bug: Switch gives back inverted value, so we also invert it with !
+                    boolean darkModeOn = !darkToggle.isChecked();
+                    System.out.println(darkModeOn);
+
+                    //saveToggle(getPreferenceManager().getContext(), !darkToggle.isChecked());
+                    if (darkModeOn) {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+                    } else {
+                        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+                    }
+
+
+                    return true;
+                }
+            });
+
+
+
         }
+
+
+
     }
+
+    private static void saveToggle(Context context, boolean isChecked) {
+        final SharedPreferences sharedPreferences = context.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean("toggle_value_theme", isChecked).apply();
+    }
+
+    private static Boolean loadToggle(Context context){
+        final SharedPreferences sharedPreferences = context.getSharedPreferences("preferences", Context.MODE_PRIVATE);
+        return sharedPreferences.getBoolean("toggle_value_theme", true);
+    }
+
 }
