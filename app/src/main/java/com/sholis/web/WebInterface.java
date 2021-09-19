f36e4a74955c4b428ec0b2fe1940a674b62ddbce
Felix Matthias Krumm <Felix.m.krumm@stud.h-da.de>
2021-09-19 23:06:10 +0200

    fix: few comments and debug

--- app/src/main/java/com/sholis/web/WebInterface.java
+++ app/src/main/java/com/sholis/web/WebInterface.java
@@ -13,6 +13,7 @@ import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.Response;
 
+import org.jetbrains.annotations.NotNull;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -27,37 +28,6 @@ import javax.net.ssl.SSLSession;
 import cz.msebera.android.httpclient.Header;
 
 public class WebInterface {
-
-    /*
-    public static void getItemsFromShoppingList(ShoppingList shoppingList, int familyId, int supermarketId, int[] syncedItems) {
-        AsyncHttpClient client = new AsyncHttpClient();
-        StringBuilder url = new StringBuilder("https://www.google.com");
-        url.append("?familyId=").append(familyId);
-        url.append("&supermarketId=").append(supermarketId);
-        client.get("https://www.google.com", new AsyncHttpResponseHandler() {
-
-            @Override
-            public void onStart() {
-                // called before request is started
-            }
-
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, byte[] response) {
-                System.out.println(new String(response));
-            }
-
-            @Override
-            public void onFailure(int statusCode, Header[] headers, byte[] errorResponse, Throwable e) {
-                // called when response HTTP status is "4XX" (eg. 401, 403, 404)
-            }
-
-            @Override
-            public void onRetry(int retryNo) {
-                // called when request is retried
-            }
-        });
-    }
-     */
 
     public static void getItemsFromShoppingList(@org.jetbrains.annotations.NotNull ShoppingList shoppingList) {
 
@@ -76,9 +46,9 @@ public class WebInterface {
             @Override
             public void onSuccess(int statusCode, Header[] headers, byte[] response) {
                 try {
-                    JSONArray result = new JSONArray(new String (response));
+                    JSONArray result = new JSONArray(new String(response));
                     ArrayList<Item> items = new ArrayList<>();
-                    for(int i = 0; i < result.length(); i++) {
+                    for (int i = 0; i < result.length(); i++) {
                         JSONObject jo = result.getJSONObject(i);
                         items.add(new Item(jo.getInt("ITEM_ID"), jo.getString("ITEM_NAME"), jo.getString("ITEM_AMOUNT")));
                     }
@@ -100,14 +70,14 @@ public class WebInterface {
         });
     }
 
-    public static String getWebData(@org.jetbrains.annotations.NotNull String URL, String parameter, @org.jetbrains.annotations.NotNull SharedPreferences sharedPreferences) {
+    public static String getWebData(@NotNull String URI, String parameter, @NotNull SharedPreferences sharedPreferences) {
 
         String webLocation = "https://krumm.ddns.net/sholis";
 
         String userName = sharedPreferences.getString("uName", "");//"No name defined" is the default value.
         String userPassword = sharedPreferences.getString("uPass", ""); //0 is the default value.
 
-        if (userName.equals("") && userPassword.equals("")) return null;
+        if (userName.equals("") && userPassword.equals("")) return "";
 
         OkHttpClient client = new OkHttpClient();
 
@@ -137,13 +107,9 @@ public class WebInterface {
             }
         });
 
-        String requestURL = webLocation + URL + parameter;
+        String requestURL = webLocation + URI + parameter;
 
         Request request = new Request.Builder().url(requestURL).build();
-
-
-
-        System.out.println("Sending request: " + request.urlString());
 
         String response = "";
         try {
@@ -152,11 +118,25 @@ public class WebInterface {
             e.printStackTrace();
         }
 
-        System.out.println(response);
+        return response;
     }
 
     public static boolean authenticateUser(String name, String password, SharedPreferences sharedPreferences) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("uName", name);
+        editor.putString("uPass", password);
+        editor.apply();
 
+        String response = getWebData("/login.php", "", sharedPreferences);
+        if (response.equals("OK")) return true;
+
+        // Delete the the wrong pass and username from preferences
+        editor = sharedPreferences.edit();
+        editor.putString("uName", "");
+        editor.putString("uPass", "");
+        editor.apply();
+
+        return false;
     }
 
 }
